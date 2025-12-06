0ba2af3413b61de683806b039c59fba9f2fcc95a
ArthurFelipePallu <arthurfelipepallu@gmail.com>
2025-12-06 10:00:45 -0300

    Posição do tabuleiro criada

---
+++ Board/ChessBoard.cs
@@ -0,0 +1,23 @@
+﻿namespace Chess_Console_Project.Board;
+
+public class ChessBoard
+{
+    private readonly int _maxChessBoardSize = 8;
+    private int[][] _board;
+    
+
+    public ChessBoard()
+    {
+        _board = new int[_maxChessBoardSize][];
+        for (var i = 0; i < _maxChessBoardSize; i++)
+        {
+            
+            for (var j = 0; j < _maxChessBoardSize; j++)
+            {
+                
+            }
+            
+        }
+        
+    }
+}
---
+++ Board/Position.cs
@@ -0,0 +1,13 @@
+﻿namespace Chess_Console_Project.Board;
+
+public class Position(int line, int column)
+{
+    public int Line { get; } = line;
+    public int Column { get; } = column;
+
+
+    public override string ToString()
+    {
+        return "L: " + Line + ", " + "C: " + Column;
+    }
+}
