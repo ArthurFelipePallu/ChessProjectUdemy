ffcc7469a2f1ca43cdd8ee009d6c8404f66752d5
ArthurFelipePallu <arthurfelipepallu@gmail.com>
2025-12-09 10:14:36 -0300

    Print Piece Possible Movements

--- Board/Pieces/PieceExtensions.cs
+++ Board/Pieces/PieceExtensions.cs
@@ -8,4 +8,23 @@ public static class PieceExtensions
     {
         return piece.GetPieceColor() == PieceColor.Black ? ConsoleColor.Black : ConsoleColor.Yellow;
     }
+    public static void PrintPiecePossibleMovesExtension(this Piece piece)
+    {
+        for (var i = 0; i < piece.Board.MaxChessBoardSize; i++)
+        {
+            piece.Board.WriteRowName(i);
+            Console.ForegroundColor = ConsoleColor.White;
+            for (var j = 0; j < piece.Board.MaxChessBoardSize; j++)
+            {
+                if(piece.IsCurrentlyAtCoordinates(i,j))
+                    Console.Write(  " 0 " );
+                else
+                    Console.Write( piece.CoordinatesIsInPossibleMoves(i,j) ? " X " : " - " );
+            }
+            Console.WriteLine(); // QUEBRA LINHA
+        }
+
+        piece.Board.WriteSeparationLine();
+        piece.Board.WriteColumnNames();
+    }
 }
